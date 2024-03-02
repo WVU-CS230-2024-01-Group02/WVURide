29d2d63e9bdf5a1d005c1db0d2cbca7975a14e1d
narogeby <nrg00007@mix.wvu.edu>
2024-03-02 12:55:13 -0500

    createaccount work done

--- wvuride/src/Components/CreateAccount.js
+++ wvuride/src/Components/CreateAccount.js
@@ -1,50 +1,38 @@
-import React from "react"
-import "./CreateAccount.css";
+
 
 function CreateAccount() {
 
-    function checkEmail(event) {
-        email = event.target.value
-        const email = document.getElementsByClassName("email");
-        at_sign = email.indexOf("@")
-        if (email.substring(at_sign + 1) != "mix.wvu.edu") {
-            const styles = StyleSheet.create({
-                input, [title = "email"]: {
-                    color: red
-                }
-            })
+
+    const [email, setEmail] = useState(props?.value ?? '');
+
+    function checkForm(event) {
+        event.preventDefault()
+        const emailToCheck = email.value
+        const domain = email.substring(email.indexOf("@") + 1)
+        const targetDomain = "mix.wvu.edu"
+        const element = document.querySelector("email")
+        if (domain !== targetDomain) {
+            element.style.color = 'red'
         }
     }
 
     return (
-        <div className="ca-container">
-            <div title="CreateAccount">
-                <h2 title="CA-Title" className="ca-title">Please create an account.</h2>
-            </div>
-            <form>
-                <div className="form-group">
-                    <label htmlFor="name">Please enter your full name:</label>
-                    <input type="text" id="name" name="name" required="required" />
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="email">Please enter your mix.wvu.edu email address.</label>
-                    <input type="text" id="email" name="email" required="required"
-                        onChange={checkEmail} />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="pfp">Upload a Profile Picture:</label>
-                    <input type="file" id="pfp" name="pfp" />
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="bio">Enter a bio if you'd like!</label>
-                    <textarea id="bio" name="bio"></textarea>
-                </div>
-                <button type="submit" className="ca-btn">Create Account</button>
-            </form>
-        </div>
-    );
+        <><div title="CreateAccount">
+            <h1 title="CA-Title">Please create an account.</h1>
+        </div><form id="CA-Fields">
+                <label>Please enter your full name:
+                    <input type="text" id="name" required="required" />
+                </label>
+                <label>Please enter your mix.wvu.edu email address.
+                    <input type="text" id="email" required="required" onInput={e => setInput(e.target.value)} />
+                </label>
+                <label>
+                    <input id="pfp" type="file" />
+                </label>
+                <label>Enter a bio if you'd like!
+                    <input type="textarea" id="bio" />
+                </label>
+                <button type="submit" id="createAccount" onClick={checkForm}>Create Account</button>
+            </form></>
+    )
 }
-
-export default CreateAccount;

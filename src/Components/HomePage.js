0ea05f9c90467247fd2702331d735c73d0b8e06b
mustafa0411 <mustafaalshebeeb@gmail.com>
2024-03-04 15:21:02 -0500

    HomePage files.

---
+++ src/Components/HomePage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./HomePage.css";
+
+
+function HomePage () {
+
+    return (
+        <div className="hp-container">
+            
+
+        </div>
+
+    );
+
+}
+
+
+export default HomePage;

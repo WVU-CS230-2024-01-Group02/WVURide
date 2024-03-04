9bdc01eb7e1b0dbb5e562cc1d8e2c6971ee2606e
mustafa0411 <mustafaalshebeeb@gmail.com>
2024-03-04 15:41:54 -0500

    New assets for homepage.

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -6,7 +6,11 @@ function HomePage () {
 
     return (
         <div className="hp-container">
-            
+            <div className="blue-rect">
+                <div className="home">
+                    <button className="home-btn"></button>
+                </div>
+            </div>
 
         </div>
 

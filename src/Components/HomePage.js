b29276e376ef1a7a0329a6423e1f04f279e7e487
mustafa0411 <mustafaalshebeeb@gmail.com>
2024-03-04 22:49:04 -0500

    Animations on buttons and search bar input implemented.

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -2,15 +2,21 @@ import React from "react";
 import "./HomePage.css";
 
 
-function HomePage () {
+function HomePage() {
     return (
         <div className="hp-container">
             <div className="blue-rect">
-                <div className="side-btns"> 
+                <div className="side-btns">
                     <button className="profile-btn"></button>
                     <button className="home-btn"></button>
                     <button className="msg-btn"></button>
                     <button className="out-btn"></button>
+                </div>
+            </div>
+            <div className="search-container">
+                <div className="search-bar">
+                    <input type="text" placeholder="Search..." className="search-input" />
+                    <button className="search-btn"></button>
                 </div>
             </div>
         </div>

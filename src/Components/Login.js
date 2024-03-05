9aee9eb7bcc9ce60db563ee58115541f29b4641c
Nathanson Gageby <nrg00007@mix.wvu.edu>
2024-03-05 06:55:52 -0500

    beginnings of login validation

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,25 +2,31 @@ import React from "react";
 import "./Login.css";
 
 function Login() {
+
+  
+
+
   return (
     <div className="login-container">
       <div className="login-function">
         <h2 className="login">Login</h2>
         <div className="login-underline"></div>
-        <form>
+        <form id='L-form' name='L-form'>
           <div className="form-group">
             <label htmlFor="username">
-              <input type="text" id="username" name="username" placeholder="Username" />
+              <input type="text" id="username" name="username" placeholder="Username" 
+              onInput={e => setUsername(e.target.value)}/>
               <div className="input-underline"></div>
             </label>
           </div>
           <div className="form-group">
             <label htmlFor="password" className="creds">
-              <input type="password" id="password" name="password" placeholder="Password" />
+              <input type="password" id="password" name="password" placeholder="Password" 
+              onInput={e => setPassword(e.target.value)}/>
               <div className="input-underline"></div>
             </label>
           </div>
-          <button type="submit" className="login-btn">Login</button>
+          <button type="submit" className="login-btn" onClick={checkLoginInfo}>Login</button>
         </form>
         <div className="links">
           <span>Don't have an account? <br /><a href="/signup">Create Account</a></span>

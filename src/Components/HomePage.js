1205e85407107fc3752ebd8529c242feecf47323
mustafa0411 <mustafaalshebeeb@gmail.com>
2024-03-04 20:20:20 -0500

    Side buttons for home page added.

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -3,20 +3,18 @@ import "./HomePage.css";
 
 
 function HomePage () {
-
     return (
         <div className="hp-container">
             <div className="blue-rect">
-                <div className="home">
+                <div className="side-btns"> 
+                    <button className="profile-btn"></button>
                     <button className="home-btn"></button>
+                    <button className="msg-btn"></button>
+                    <button className="out-btn"></button>
                 </div>
             </div>
-
         </div>
-
     );
-
 }
-
 
 export default HomePage;

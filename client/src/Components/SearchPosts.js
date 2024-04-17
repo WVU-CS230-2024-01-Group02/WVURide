4ba8e813471998f141259dec44f323687ac43122
narogeby <nrg00007@mix.wvu.edu>
2024-04-17 10:39:32 -0400

    searchposts now properly gets posts and should display them

--- client/src/Components/SearchPosts.js
+++ client/src/Components/SearchPosts.js
@@ -18,101 +18,92 @@ function Post(props) {
     );
 }
 
-const Profile = () => {
-    const[to, setTo] = useState("")
-    const[from, setFrom] = useState("")
-    const[gas, setGas] = useState(-1)
+const searchPosts = () => {
+    const [to, setTo] = useState("")
+    const [from, setFrom] = useState("")
+    const [gas, setGas] = useState(-1)
     var postResponse
 
-    async function searchPosts(){
-        if(to == null && from == null && gas == -1){
-            const response = await axios.get("http://localhost:8800/retrieve5Posts").then(response => {
-                const postItems = response.body.map((post) => 
-                    <Post key={response.body.id}
-                        text={response.body.postDesc}
-                        time={response.body.postTime}
-                        userName={response.body.postAuth}
-                        to={response.body.postTo}
-                        from={response.body.postFrom}
-                        title={response.body.postTitle}
-                    />
-                )
-                return postItems
-                }).catch(error => {
-                    if (error.status !== 200){
-                        return null
-                    }
-                });
+    const [posts, setPosts] = useState({ loaded: false, data: [] })
+
+    const fetchPosts = useCallback(async () => {
+        const response = await axios.get("http://localhost:8800/retrieve5Posts").then(response => {
+            return response.data
+        }).catch(error => {
+            if (error.status !== 200) {
+                return null
+            }
+        });
+        setPosts({ loaded: true, data: response })
+    }, [setPosts])
+
+    const fetchPostsByFrom = useCallback(async () => {
+        const response = await axios.get("http://localhost:8800/findPostByFrom").then(response => {
+            return response.data
+        }).catch(error => {
+            if (error.status !== 200) {
+                return null
+            }
+        });
+        setPosts({ loaded: true, data: response })
+    }, [setPosts])
+
+    const fetchPostsByTo = useCallback(async () => {
+        const response = await axios.get("http://localhost:8800/findPostByTo").then(response => {
+            return response.data
+        }).catch(error => {
+            if (error.status !== 200) {
+                return null
+            }
+        });
+        setPosts({ loaded: true, data: response })
+    }, [setPosts])
+
+    const fetchPostsByToAndFrom = useCallback(async () => {
+        const response = await axios.get("http://localhost:8800/findPostByToAndFrom").then(response => {
+            return response.data
+        }).catch(error => {
+            if (error.status !== 200) {
+                return null
+            }
+        });
+        setPosts({ loaded: true, data: response })
+    }, [setPosts])
+
+    async function searchForPosts() {
+        if (to == null && from == null && gas == -1) {
+            useEffect(() => {
+                if (!posts.loaded) {
+                    fetchPosts()
+                }
+            }, [fetchPosts, posts.loaded])
         }
-        else if(from == null && gas == -1 && to != null){
-            const response = await axios.get("http://localhost:8800/findPostByTo", {
-                searchTo: to,
-            }).then(response => {
-                const postItems = response.body.map((post) => 
-                    <Post key={response.body.id}
-                        text={response.body.postDesc}
-                        time={response.body.postTime}
-                        userName={response.body.postAuth}
-                        to={response.body.postTo}
-                        from={response.body.postFrom}
-                        title={response.body.postTitle}
-                    />
-                )
-                return postItems
-                }).catch(error => {
-                    if (error.status !== 200){
-                        return null
-                    }
-                });
+        else if (from == null && gas == -1 && to != null) {
+            useEffect(() => {
+                if (!posts.loaded) {
+                    fetchPostsByTo()
+                }
+            }, [fetchPostsByTo, posts.loaded])
         }
-        else if(to == null && gas == -1 && from != null){
-            const response = await axios.get("http://localhost:8800/findPostByFrom", {
-                searchFrom: from,
-            }).then(response => {
-                const postItems = response.body.map((post) => 
-                    <Post key={response.body.id}
-                        text={response.body.postDesc}
-                        time={response.body.postTime}
-                        userName={response.body.postAuth}
-                        to={response.body.postTo}
-                        from={response.body.postFrom}
-                        title={response.body.postTitle}
-                    />
-                )
-                return postItems
-                }).catch(error => {
-                    if (error.status !== 200){
-                        return null
-                    }
-                });
+        else if (to == null && gas == -1 && from != null) {
+            useEffect(() => {
+                if (!posts.loaded) {
+                    fetchPostsByFrom()
+                }
+            }, [fetchPostsByFrom, posts.loaded])
         }
-        else if(gas == -1 && from != null && to != null){
-            const response = await axios.get("http://localhost:8800/findPostByFromAndTo", {
-                searchTo: to,
-                searchFrom: from,
-            }).then(response => {
-                const postItems = response.body.map((post) => 
-                    <Post key={response.body.id}
-                        text={response.body.postDesc}
-                        time={response.body.postTime}
-                        userName={response.body.postAuth}
-                        to={response.body.postTo}
-                        from={response.body.postFrom}
-                        title={response.body.postTitle}
-                    />
-                )
-                return postItems
-                }).catch(error => {
-                    if (error.status !== 200){
-                        return null
-                    }
-                });
+        else if (gas == -1 && from != null && to != null) {
+            useEffect(() => {
+                if (!posts.loaded) {
+                    fetchPostsByToAndFrom()
+                }
+            }, [fetchPostsByToAndFrom, posts.loaded])
         }
     }
 
-  return (
-    <div className='search-posts-container'>
-        <div className="blue-rect">
+    return (
+        <div className='search-posts-container'>
+            <div className="blue-rect">
                 <div className="side-btns">
                     <Link to="/profile"><button className="profile-btn"></button></Link>
                     <Link to="/home"><button className="home-btn"></button></Link>
@@ -121,22 +112,22 @@ const Profile = () => {
                     <Link to="/post"><button className="post-btn"></button></Link>
                 </div>
             </div>
-        <div className="searching">
-            <div className="to-and-from">
-                <input type="text" className="search-from" placeholder="From:" required onInput={e => setFrom(e.target.value)}></input>
-                <input type="text" className="search-to" placeholder="To:" required onInput={e => setTo(e.target.value)}></input>
-            </div>
-            <div className="filter-btns">
-              <label>
-                <input type="checkbox" name="gas" className="gas" onInput={e => setGas(1)} />
-                <span className="flag-button gas" >Gas</span>
-              </label>
-              <label>
-                <input type="checkbox" name="no-gas" className="no-gas" onInput={e => setGas(0)} />
-                <span className="flag-button no-gas">No Gas</span>
-              </label>
-            </div>
-            <button type="submit" className="search-post-btn" OnClick={e => searchPosts()}>Search</button>
+            <div className="searching">
+                <div className="to-and-from">
+                    <input type="text" className="search-from" placeholder="From:" required onInput={e => setFrom(e.target.value)}></input>
+                    <input type="text" className="search-to" placeholder="To:" required onInput={e => setTo(e.target.value)}></input>
+                </div>
+                <div className="filter-btns">
+                    <label>
+                        <input type="checkbox" name="gas" className="gas" onInput={e => setGas(1)} />
+                        <span className="flag-button gas" >Gas</span>
+                    </label>
+                    <label>
+                        <input type="checkbox" name="no-gas" className="no-gas" onInput={e => setGas(0)} />
+                        <span className="flag-button no-gas">No Gas</span>
+                    </label>
+                </div>
+                <button type="submit" className="search-post-btn" OnClick={e => searchForPosts()}>Search</button>
             </div>
             <div className="post-container">
                 <div className="title-container">
@@ -145,11 +136,19 @@ const Profile = () => {
                 </div>
 
                 <ul className="post-list">
-                    {postResponse}
+                    {posts.loaded && posts.data.length > 0 ? posts.data.map(post => {
+                        console.log(post)
+                        return <Post
+                            text={post.postDesc}
+                            time={post.postTime}
+                            userName={post.postAuth}
+                            title={post.postTitle}
+                        />
+                    }) : null}
                 </ul>
             </div>
-    </div>
-    
-    
-  )
+        </div>
+
+
+    )
 }

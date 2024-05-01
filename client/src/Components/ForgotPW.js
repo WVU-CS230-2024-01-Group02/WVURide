fea91d83b68d08646817fbb66ab88e466eda3fae
narogeby <nrg00007@mix.wvu.edu>
2024-04-30 22:49:49 -0400

    fixed forgot pw problem where the page wouldnt show up

--- client/src/Components/ForgotPW.js
+++ client/src/Components/ForgotPW.js
@@ -34,57 +34,56 @@ const ForgotPW = () => {
     };
 
 
-    const ForgotPW = () => {
 
-        return (
-            <div className='fp-container'>
-                <div className="fp-function">
-                    <h2 className='forgot'>Reset Password</h2>
-                    <div className="fp-underline"></div>
-                    <p className='fp-desc'>Enter your email and your new password in order to reset your current password</p>
-                    <form className='FP-form'>
+    return (
+        <div className='fp-container'>
+            <div className="fp-function">
+                <h2 className='forgot'>Reset Password</h2>
+                <div className="fp-underline"></div>
+                <p className='fp-desc'>Enter your email and your new password in order to reset your current password</p>
+                <form className='FP-form'>
 
-                        <div className='form-group'>
-                            <label htmlFor='email'>
-                                <input type="email" id="email" name="email" required="required" placeholder="Mix Email" />
-                                <div className="input-underline" />
-                            </label>
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="password"></label>
-                            <input type="password" id="password" name="password" required="required" placeholder="Password"></input>
-                            <div className="input-underline"></div>
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="confirm"></label>
-                            <input type="password" id="confirm" name="confirm" required="required" placeholder="Confirm your password"></input>
-                            <div className="input-underline"></div>
-                        </div>
-                        <button type="submit" className="ca-btn" onClick={changePass}>Reset your password</button>
-
-                    </form>
-                    <div className="links">
-                        <span><Link to="/"><a href="/login">Go back to login.</a></Link></span>
+                    <div className='form-group'>
+                        <label htmlFor='email'>
+                            <input type="email" id="email" name="email" required="required" placeholder="Mix Email" />
+                            <div className="input-underline" />
+                        </label>
                     </div>
-                    <div className="map-container-ca">
-                        <div className="map-design-ca">
-                            <div className="blue-bg-ca">
-                            </div>
-                        </div>
-                        <div className="wvu-logo-ca"></div>
+
+                    <div className="form-group">
+                        <label htmlFor="password"></label>
+                        <input type="password" id="password" name="password" required="required" placeholder="Password"></input>
+                        <div className="input-underline"></div>
                     </div>
+
+                    <div className="form-group">
+                        <label htmlFor="confirm"></label>
+                        <input type="password" id="confirm" name="confirm" required="required" placeholder="Confirm your password"></input>
+                        <div className="input-underline"></div>
+                    </div>
+                    <button type="submit" className="ca-btn" onClick={changePass}>Reset your password</button>
+
+                </form>
+                <div className="links">
+                    <span><Link to="/"><a href="/login">Go back to login.</a></Link></span>
                 </div>
-                <div className="map-container-fp">
-                    <div className="map-design-fp">
-                        <div className="blue-bg-fp">
+                <div className="map-container-ca">
+                    <div className="map-design-ca">
+                        <div className="blue-bg-ca">
                         </div>
                     </div>
-                    <div className="wvu-logo-fp"></div>
+                    <div className="wvu-logo-ca"></div>
                 </div>
             </div>
-        )
-    }
+            <div className="map-container-fp">
+                <div className="map-design-fp">
+                    <div className="blue-bg-fp">
+                    </div>
+                </div>
+                <div className="wvu-logo-fp"></div>
+            </div>
+        </div>
+    )
 }
+
 export default ForgotPW;
